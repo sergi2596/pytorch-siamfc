264039846bb074b6e2a632d7fe95ab47345eaa8d
Sergi Sanchez Deutsch <s.s.deutsch72@gmail.com>
2018-11-12 09:16:38 +0100

    Created Repo structure, added example script

---
+++ modified-alexnet/modified-alexnet.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+
+__all__ = ['modifiedAlexNet', 'modifiedalexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+
+class modifiedAlexNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(modifiedAlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(48, 256, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 384, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 256, kernel_size=3)
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
+def modifiedalexnet(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = modifiedAlexNet(**kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
+    return model
